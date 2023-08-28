9ac8f656f41ab100d1e21c7bdb7b02023a9844f4
Vincent Delitz <vincent.delitz@corbado.com>
2023-08-28 23:00:48 +0200

    change order of imports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Profile from "./Profile";
 import Home from "./Home";
+import Profile from "./Profile";
 
 const router = createBrowserRouter([
 
