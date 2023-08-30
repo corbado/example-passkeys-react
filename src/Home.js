772220045d9f67d9318a4ef045726f2106e16e20
Vincent Delitz <vincent.delitz@corbado.com>
2023-08-30 17:57:12 +0200

    add import

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
-import '@corbado/webcomponent/pkg/auth_cui.css'
+import '@corbado/webcomponent/pkg/auth_cui.css';
+import '@corbado/webcomponent';
 
 const CORBADO_PROJECT_ID = process.env.REACT_APP_CORBADO_PROJECT_ID;
 
