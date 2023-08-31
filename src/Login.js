dc9ffb448846f37bf72a15af1bcb6a7bd81111d8
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-08-31 14:10:49 +0200

    Added a proper homepage for docs

---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import '@corbado/webcomponent/pkg/auth_cui.css';
+import '@corbado/webcomponent';
+
+const CORBADO_PROJECT_ID = process.env.REACT_APP_CORBADO_PROJECT_ID;
+
+function Login() {
+    return (
+        <div className="App">
+            <corbado-auth project-id={CORBADO_PROJECT_ID} conditional="yes">
+                <input name="username" id="corbado-username"
+                       required autoComplete="webauthn"/>
+            </corbado-auth>
+        </div>
+       );
+    }
+
+export default Login;
